20e7e18129120f8934692651ff6447477e67354e
Toyehamzat <anibiseun@gmail.com>
2024-06-25 16:43:33 +0100

    feat: Add Post model for storing blog posts

---
+++ server/models/post.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minLength: 1,
+    },
+    description: {
+      type: String,
+      required: true,
+      minLength: 1,
+    },
+    content: {
+      type: String,
+      required: true,
+      minLength: 1,
+    },
+    // imageUrl: { type: String, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
