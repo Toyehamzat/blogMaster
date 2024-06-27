9b175f5e2eec1146a953bc259a3cbb454a5dfadf
Toyehamzat <anibiseun@gmail.com>
2024-06-27 13:54:05 +0100

    chore: Update signup page with form layout and styling

--- client/app/(platform)/(auth)/signup/page.tsx
+++ client/app/(platform)/(auth)/signup/page.tsx
@@ -1,7 +1,47 @@
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <section className="flex flex-col max-w-xl p-8 mx-auto mt-8">
+        <form className="space-y-8 mt-8">
+          <div className="form-item">
+            <label className="form-label">Username</label>
+            <input
+              className="form-control input"
+              type="text"
+              required
+              disabled={false}
+            />
+            <div className="form-message"></div>
+          </div>
+          <div className="form-item">
+            <label className="form-label">Password</label>
+            <input
+              className="form-control input"
+              type="password"
+              required
+              disabled={false}
+            />
+            <div className="form-message"></div>
+          </div>
+          <div className="form-item">
+            <label className="form-label">Confirm Password</label>
+            <input
+              className="form-control input"
+              type="password"
+              required
+              disabled={false}
+            />
+            <div className="form-message"></div>
+          </div>
+          <button className="button" type="submit" disabled={false}>
+            Sign up
+          </button>
+        </form>
+      </section>
+    </div>
+  );
 }
 
 export default page;
