756cbc45f020694ecf7baecc383881498cd56789
Toyehamzat <anibiseun@gmail.com>
2024-06-27 13:53:48 +0100

    feat: Add signup form validation schema

---
+++ client/action/signup/schema.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+export const SignUpSchema = z
+  .object({
+    username: z
+      .string()
+      .min(3, "Username must consist of at least 3 characters")
+      .nonempty("Username cannot be empty!")
+      .trim(),
+    email: z
+      .string()
+      .email("Please provide a valid email")
+      .nonempty("Email cannot be empty!")
+      .trim(),
+    password: z
+      .string()
+      .min(6, "Password must consist of at least 6 characters")
+      .nonempty("Password cannot be empty!")
+      .trim(),
+    confirm_password: z
+      .string()
+      .nonempty("Confirm Password cannot be empty!")
+      .trim(),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: "Passwords must be the same",
+    path: ["confirm_password"],
+  });
