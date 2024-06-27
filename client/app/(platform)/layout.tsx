9b13ffe9b33962d010e832ec1e84a90af9e94339
Toyehamzat <anibiseun@gmail.com>
2024-06-27 15:48:56 +0100

    feat: Add PlatformLayout component for platform-specific layout

---
+++ client/app/(platform)/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import { Toaster } from "sonner";
+export const metadata: Metadata = {
+  title: "platform ",
+};
+
+export default function PlatformLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-full">
+      {children}
+      <Toaster />
+    </div>
+  );
+}
