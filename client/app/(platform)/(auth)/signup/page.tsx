b30c617de88d74b13bfe58bf7bc699fd697eb4eb
Toyehamzat <anibiseun@gmail.com>
2024-06-28 16:46:12 +0100

    yh

--- client/app/(platform)/(auth)/signup/page.tsx
+++ client/app/(platform)/(auth)/signup/page.tsx
@@ -103,7 +103,7 @@ export default function SignUp({ data }: SignUpProps) {
               htmlFor="confirm_password"
               className="block text-sm font-medium text-gray-700"
             >
-              Confirm Passwords
+              Confirm Password
             </label>
             <input
               id="confirm_password"
