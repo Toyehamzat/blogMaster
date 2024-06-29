6eaf1ec7d137cba9e0eefd4a1ed59eebf4142a73
Toyehamzat <anibiseun@gmail.com>
2024-06-29 13:10:34 +0100

    feat: Add login types for user authentication

---
+++ client/action/login/types.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+import { ActionState } from "@/lib/create-safe-action";
+
+import { LoginSchema } from "./schema";
+
+export type InputType = z.infer<typeof LoginSchema>;
+export type ReturnType = ActionState<
+  InputType,
+  { token: string; message: string }
+>;
