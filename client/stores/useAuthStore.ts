7538a0e39fafb19d09d822fa8a128d8efc8e0874
Toyehamzat <anibiseun@gmail.com>
2024-06-29 13:29:17 +0100

    feat: Add user authentication store

---
+++ client/stores/useAuthStore.ts
@@ -0,0 +1,29 @@
+import create from "zustand";
+import { persist } from "zustand/middleware";
+
+export interface UserAuthStore {
+  isLoggedIn: boolean;
+  userLogin: () => void;
+  logout: () => void;
+}
+
+export const useUserStore = create(
+  persist<UserAuthStore>(
+    (set) => ({
+      isLoggedIn: false,
+      userLogin: () => {
+        const userLocalStorage = localStorage.getItem("token");
+        if (userLocalStorage) {
+          set({ isLoggedIn: true });
+        }
+      },
+      logout: () => {
+        set({ isLoggedIn: false });
+        localStorage.clear();
+      },
+    }),
+    {
+      name: "userStore",
+    }
+  )
+);
