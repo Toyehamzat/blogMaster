5d8954bdcbe0da46097a3a9e1b98f3ec93766772
Toyehamzat <anibiseun@gmail.com>
2024-08-22 12:56:51 +0100

    refactor: Remove commented out code for Advert component in MarketingLayout

--- client/app/(marketing)/layout.tsx
+++ client/app/(marketing)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Navbar } from "./_components/navbar";
-// import { Advert } from "./_components/advert";
 import { Footer } from "./_components/footer";
 
 export const metadata: Metadata = {
@@ -15,7 +14,6 @@ export default function MarketingLayout({
   return (
     <div className="h-full bg-slate-100">
       <Navbar />
-      {/* <Advert /> */}
       <main className="pt-56 pb-20 bg-slate-100">{children}</main>
       <Footer />
     </div>
