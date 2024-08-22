178038d48f55ecaac6bfa4e6735cb6c9ba583d08
Toyehamzat <anibiseun@gmail.com>
2024-08-22 12:56:41 +0100

    refactor: Update login and signup action types
    This commit updates the login and signup action types to include the new properties returned by the server. The "token" property has been renamed to "accessToken", and the "message" property has been replaced with "refreshToken" and "email". This ensures that the action types accurately reflect the data structure returned by the server.

--- client/action/login/types.ts
+++ client/action/login/types.ts
@@ -1,18 +1,18 @@
 import { z } from "zod";
-
 import { ActionState } from "@/lib/create-safe-action";
-
 import { LoginSchema } from "./schema";
 
 export type InputType = z.infer<typeof LoginSchema>;
+
 export type ReturnType = ActionState<
   InputType,
   {
-    token: string;
-    message: string;
+    accessToken: string;
+    refreshToken: string;
     user: {
       id: string;
       username: string;
+      email: string;
     };
   }
 >;
--- client/action/signup/types.ts
+++ client/action/signup/types.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
-
 import { ActionState } from "@/lib/create-safe-action";
-
 import { SignUpSchema } from "./schema";
 
 export type InputType = z.infer<typeof SignUpSchema>;
+
 export type ReturnType = ActionState<InputType, { message: string }>;
