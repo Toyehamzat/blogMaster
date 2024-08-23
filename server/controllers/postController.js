92197e6592493e0434d718b6cec426fb90f24295
Toyehamzat <anibiseun@gmail.com>
2024-08-23 14:34:44 +0100

    refactor: Update getAllPosts route to populate author username
    This commit updates the getAllPosts route in the server/controllers/postController.js file to populate the "author" field with the "username" property. This change enhances the functionality of the route by including the username of the post author in the response, providing more information about the posts.

---
+++ server/controllers/postController.js
@@ -0,0 +1,184 @@
+const { body, param, validationResult } = require("express-validator");
+const Post = require("../models/post");
+const Comment = require("../models/comment");
+
+// Get all posts
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().populate("author", "username");
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch posts" });
+  }
+};
+
+// Get post by ID
+const getPostById = [
+  param("id").isMongoId().withMessage("Invalid post ID"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const post = await Post.findById(req.params.id).populate(
+        "author",
+        "username"
+      );
+      if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+      }
+      res.status(200).json(post);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch post" });
+    }
+  },
+];
+
+// Get latest posts
+const getLatestPosts = async (req, res) => {
+  try {
+    const latestPosts = await Post.find()
+      .sort({ createdAt: -1 })
+      .limit(5)
+      .populate("author", "username");
+    res.status(200).json(latestPosts);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch latest posts" });
+  }
+};
+
+// Create a new post
+const createPost = [
+  async (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    next();
+  },
+  body("title").notEmpty().withMessage("Title is required"),
+  body("description").notEmpty().withMessage("Description is required"),
+  body("content").notEmpty().withMessage("Content is required"),
+  body("imageUrl").notEmpty().withMessage("Image URL is required"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const newPost = new Post({
+        ...req.body,
+        author: req.user._id,
+      });
+      const savedPost = await newPost.save();
+      res.status(201).json(savedPost);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to create post" });
+    }
+  },
+];
+
+// Delete a post
+const deletePost = [
+  async (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    next();
+  },
+  param("id").isMongoId().withMessage("Invalid post ID"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+      }
+
+      if (post.author.toString() !== req.user._id.toString()) {
+        return res
+          .status(403)
+          .json({ error: "Forbidden: You are not the author of this post" });
+      }
+
+      await post.deleteOne();
+      await Comment.deleteMany({ post: req.params.id });
+
+      res
+        .status(200)
+        .json({ message: "Post and associated comments deleted successfully" });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Failed to delete post and associated comments" });
+    }
+  },
+];
+
+// Update a post
+const updatePost = [
+  async (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    next();
+  },
+  param("id").isMongoId().withMessage("Invalid post ID"),
+  body("title").optional().notEmpty().withMessage("Title cannot be empty"),
+  body("description")
+    .optional()
+    .notEmpty()
+    .withMessage("Description cannot be empty"),
+  body("content").optional().notEmpty().withMessage("Content cannot be empty"),
+  body("imageUrl")
+    .optional()
+    .notEmpty()
+    .withMessage("Image URL cannot be empty"),
+  body("published")
+    .optional()
+    .isBoolean()
+    .withMessage("Published must be a boolean"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+      }
+
+      if (post.author.toString() !== req.user._id.toString()) {
+        return res
+          .status(403)
+          .json({ error: "Forbidden: You are not the author of this post" });
+      }
+
+      const updatedPost = await Post.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true, runValidators: true }
+      );
+      res.status(200).json(updatedPost);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to update post" });
+    }
+  },
+];
+
+module.exports = {
+  getAllPosts,
+  getPostById,
+  getLatestPosts,
+  createPost,
+  deletePost,
+  updatePost,
+};
