8ec36798441c69b3a772c564411641dcc163adb8
Toyehamzat <anibiseun@gmail.com>
2024-08-23 14:34:09 +0100

    refactor: Add CRUD routes for posts in API
    This commit adds CRUD routes for posts in the API. It includes routes for getting all posts, getting a post by ID, getting the latest posts, creating a new post, deleting a post, and updating a post. These routes are protected and require authentication. This update enhances the functionality of the API by providing the necessary routes for managing posts.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,28 @@
-// routes/api.js
 const express = require("express");
 const { signup, login, logout } = require("../controllers/authController");
+const {
+  getAllPosts,
+  getPostById,
+  getLatestPosts,
+  createPost,
+  deletePost,
+  updatePost,
+} = require("../controllers/postController");
+const protectedRoute = require("../middleware/protectedRoute");
+
 const router = express.Router();
 
+// Auth routes
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+// Post routes
+router.get("/posts", getAllPosts); // Get all posts
+router.get("/latest-posts", getLatestPosts); // Get latest posts
+router.get("/posts/:id", getPostById); // Get a post by ID
+router.post("/create-posts", protectedRoute, createPost); // Create a new post
+router.delete("/delete-posts/:id", protectedRoute, deletePost); // Delete a post
+router.put("/update-posts/:id", protectedRoute, updatePost); // Update a post
 
 module.exports = router;
