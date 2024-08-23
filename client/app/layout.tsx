5eed52930ec0fdd5421ad318b9024c2b7aa7b2ac
Toyehamzat <anibiseun@gmail.com>
2024-08-23 14:35:43 +0100

    refactor: Add Toaster component to layout for displaying notifications
    This commit adds the Toaster component to the layout file in order to display notifications to the user. The Toaster component is imported and rendered within the body element, allowing for easy display of toast notifications throughout the application. This enhancement improves the user experience by providing a consistent and visually appealing way to notify the user of important events.

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "BlogMaster",
@@ -14,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children} <Toaster />
+      </body>
     </html>
   );
 }
