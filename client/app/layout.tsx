6c8036b144aa5796e7fef2ec3d7021ecbc3185ef
Toyehamzat <anibiseun@gmail.com>
2024-08-24 12:50:38 +0100

    refactor: Update layout to include Navbar component
    This commit updates the layout file in the client app to include the Navbar component from the (marketing) section. The Navbar component is imported and rendered within the body element, allowing for easy navigation throughout the application. This enhancement improves the user experience by providing a consistent and convenient way to navigate between pages.

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 
@@ -16,7 +15,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children} <Toaster />
+        {children}
+        <Toaster />
       </body>
     </html>
   );
