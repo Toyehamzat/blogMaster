2f6b5fdb9d67da341617eb576c8f859b33f803a7
Toyehamzat <anibiseun@gmail.com>
2024-08-24 12:51:54 +0100

    refactor: Update navbar to include user dropdown menu
    This commit updates the navbar component in the marketing section to include a user dropdown menu for logged-in users. The dropdown menu allows users to access their account information, create a new post, and log out. This enhancement improves the user experience by providing a more intuitive and convenient way to interact with user-related actions.

--- client/app/(marketing)/_components/navbar.tsx
+++ client/app/(marketing)/_components/navbar.tsx
@@ -1,42 +1,76 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
 import { useUserStore } from "@/stores/useAuthStore";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
 import LogoutButton from "./logOutbtn";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DoorClosed, LogOut, LogOutIcon, PenBox, User } from "lucide-react";
 
 export const Navbar = () => {
   const { isLoggedIn, user } = useUserStore((state) => state);
   const router = useRouter();
 
   return (
-    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+    <nav className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-grey flex items-center ">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
-        <div className="font-extrabold">BlogMaster</div>
-        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+        <Link
+          href="/"
+          className="text-[22px] font-extrabold border-b-4 tracking-tighter"
+        >
+          BlogMaster
+        </Link>
+        <div>
           {isLoggedIn ? (
-            <>
-              <Button>Create post</Button>
-              <LogoutButton />
-            </>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="w-12 h-12 font-semibold text-xl rounded-full"
+                >
+                  {user?.username.charAt(0).toUpperCase()}
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="center">
+                <DropdownMenuItem className="flex text-center gap-1">
+                  <User size={15} />
+                  my account
+                </DropdownMenuItem>
+                <hr />
+                <DropdownMenuItem
+                  className="flex text-center gap-1"
+                  onClick={() => router.push("/create-post")}
+                >
+                  <PenBox size={15} />
+                  Create post
+                </DropdownMenuItem>
+                <hr />
+                <DropdownMenuItem className="flex text-center gap-1">
+                  <LogOutIcon size={14} />
+                  <LogoutButton />
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           ) : (
             <>
-              <Button size="sm" variant="outline" asChild>
+              <Button variant="ghost" className="mr-2" asChild>
                 <Link href="/login">Login</Link>
               </Button>
-              <Button
-                className="bg-amber-700 hover:bg-amber-800 transition"
-                size="sm"
-                asChild
-              >
+              <Button asChild>
                 <Link href="/signup">Sign up for free</Link>
               </Button>
             </>
           )}
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
