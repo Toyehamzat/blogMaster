21b54a9ae33445b3ec338cde7bfa766941750d01
Toyehamzat <anibiseun@gmail.com>
2024-08-24 13:06:15 +0100

    refactor: Update layout to include heading font and fix styling
    This commit updates the layout file in the client app to include the heading font and fix the styling issues. The heading font is imported from the local font file and applied to the appropriate elements using the "headingFont" class. Additionally, the styling of the welcome message is adjusted to use the "font-medium" class instead of "font-semibold" for better consistency. These changes improve the visual appearance and overall design of the page.

--- client/app/(marketing)/page.tsx
+++ client/app/(marketing)/page.tsx
@@ -1,29 +1,20 @@
-import { Button } from "@/components/ui/button";
-import { Medal } from "lucide-react";
-import Link from "next/link";
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
-import { Poppins } from "next/font/google";
 
-// const headingFont = localFont({
-//   src: "../../public/fonts/font.woff2",
-// });
-
-// const textFont = Poppins({
-//   subsets: ["latin"],
-//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-// });
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2",
+});
 
 export default function Home() {
   return (
     <main className="pt-48 md:max-w-screen-2xl m-auto  flex flex-col justify-center overflow-hidden">
       <div
         className={cn(
-          "flex items-center justify-center flex-col "
-          //   headingFont.className
+          "flex items-center justify-center flex-col",
+          headingFont.className
         )}
       >
-        <div className="mb-4 flex flex-row items-center border shadow-sm p-4 rounded-full bg-amber-100 text-amber-700 uppercase font-semibold">
+        <div className="mb-4 flex flex-row items-center border shadow-sm p-4 rounded-full bg-amber-100 text-amber-700 uppercase font-medium">
           Welcome to Tazmaheyot&apos;s Blog
         </div>
         <h1 className="max-w-3xl text-3xl md:text-7xl  text-neutral-800 mb-6 text-center">
