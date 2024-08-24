31d2a2cefdcba6cce3c49607f1be957518accfb0
Toyehamzat <anibiseun@gmail.com>
2024-08-24 13:06:05 +0100

    refactor: Update layout to include Poppins font for text
    This commit updates the layout file in the client app to include the Poppins font for text. The font is imported from the "next/font/google" package and applied to the body element using the "textFont" class. This enhancement improves the visual appearance of the text throughout the application, providing a more modern and stylish look.

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Poppins } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "BlogMaster",
   description: "blog",
 };
 
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -14,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={textFont.className}>
         {children}
         <Toaster />
       </body>
