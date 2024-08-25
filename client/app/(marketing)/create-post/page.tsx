09837dbe1c9204ad8a98121903c5f02d99ed29c6
Toyehamzat <anibiseun@gmail.com>
2024-08-25 22:32:20 +0100

    imports

--- client/app/(marketing)/create-post/page.tsx
+++ client/app/(marketing)/create-post/page.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 
+import { useRouter } from "next/navigation";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useForm, Controller } from "react-hook-form";
+import { toast } from "sonner";
+
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+import "react-markdown-editor-lite/lib/index.css";
+
 export default function CreatePostPage() {
   return (
     <div>
-      <div className="w-full text-3xl font-semibold">Write a new post.</div>
+      <div className="w-full text-3xl font-medium">Write a new post.</div>
+      <div></div>
     </div>
   );
 }
